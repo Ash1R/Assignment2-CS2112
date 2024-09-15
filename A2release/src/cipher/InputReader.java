a4642ffc29c2c19332db3e01983156e1fb49c6dd
Ashir Rao <69091220+Ash1R@users.noreply.github.com>
2024-09-14 20:03:07 -0400

    started working on implemeting ChunkReader

--- A2release/src/cipher/InputReader.java
+++ A2release/src/cipher/InputReader.java
@@ -1,7 +1,32 @@
 package cipher;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.EOFException;
+import java.io.Reader;
+
+
 public class InputReader implements ChunkReader{
+
+    private final InputStream stream;
+    private final int chunkSize;
+    private boolean moreBytes = true;
+
+    }
+
+    public InputReader(InputStream stream, int chunkSize){
+        this.stream = new InputStream(stream);
+        this.chunkSize = chunkSize;
+    }
+
+    @Override
     public int chunkSize(){
-        
+        return chunkSize;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return moreBytes;
     }
 }
