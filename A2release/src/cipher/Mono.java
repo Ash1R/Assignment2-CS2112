363b0f3696b4e0299b57d3544f341be1186ceae3
G33-Man <gfs64@cornell.edu>
2024-09-17 03:13:02 -0400

    Create Mono.java

---
+++ A2release/src/cipher/Mono.java
@@ -0,0 +1,12 @@
+package cipher;
+
+import java.io.*;
+
+public abstract class Mono implements cipher{
+
+    public static String[] alphabet = new String[]{"a", "b", "c", "d", "e", "f", "g",
+            "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "q",
+            "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+
+}
